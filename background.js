64d31f3076247e0e8a73026b52883be7d4e27d36
razorcx-courses <100399176+razorcx-courses@users.noreply.github.com>
2024-03-05 14:53:17 -0800

    remove unused code and files

--- background.js
+++ background.js
@@ -1,8 +1,6 @@
 //https://www.semrush.com/blog/most-visited-websites/
 
 chrome.cookies.onChanged.addListener((changeInfo) => {
-  console.log("**** COOKIE CHANGE ****", changeInfo);
-
   if (!changeInfo.removed) {
     chrome.cookies.remove({
       url: "http://" + changeInfo.cookie.domain + changeInfo.cookie.path,
@@ -11,35 +9,14 @@ chrome.cookies.onChanged.addListener((changeInfo) => {
   }
 });
 
-const getTab = async () => {
-  const [tab] = await chrome.tabs.query({ active: true });
-  return tab;
-};
-
-chrome.action.onClicked.addListener(async (tab) => {
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    args: [tab],
-    func: (tab) => {
-      console.log("**** Extension Clicked ****", tab.id);
-    },
-  });
-});
-
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     args: [tab],
     func: (tab) => {
-      // console.log("**** Tab Updated ****", tab.id);
-      console.log("**** AD Clean-Up Script ****");
-
       const adDivs = document.querySelectorAll(
-        'div[class*="ad-wrapper"], div[id*="ad-wrapper"], div[id*="adchoicesBtn"], div[id*="zdcFloatingBtn"], div[class*="glacier-ad"], div[class*="video_player"], div[class*="ad--container"], div[class*="ad__slot"], div[class*="ad-slot"], div[class*="container__ads"], div[class*="dmg-ads"], div[class*="stack__ads"], div[class*="adSlug"], div[class*="display-ads-container"], div[class*="ads-container"], div[class*="billboard-container"]'
+        'div[class*="ad-wrapper"], div[id*="ad-wrapper"], div[id*="adobe-analytics"], div[id*="leaderboard-wrap"], div[id*="leaderboard"], div[id*="adchoicesBtn"], div[id*="zdcFloatingBtn"], div[class*="AdsWrapper"], div[class*="HeaderAds"], div[class*="glacier-ad"], div[class*="video_player"], div[class*="ad--container"], div[class*="ad__slot"], div[class*="ad-slot"], div[class*="container__ads"], div[class*="dmg-ads"], div[class*="stack__ads"], div[class*="adSlug"], div[class*="display-ads-container"], div[class*="ads-container"], div[class*="billboard-container"]'
       );
-
-      // Log the found elements to the console
-      adDivs.forEach((div) => console.log(div));
 
       adDivs.forEach((div) => {
         if (div.parentNode) {
@@ -47,18 +24,11 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
         }
       });
 
-      // Get all <div> elements in the document
       const divs = document.querySelectorAll("div");
 
-      // Filter <div> elements that have inner text "advertisement"
       const adDivs2 = Array.from(divs).filter(
         (div) => div.innerText.trim() === "Advertisement"
       );
-
-      // Do something with the filtered <div> elements
-      adDivs2.forEach((div) => {
-        console.log(div); // For example, log each matching <div> to the console
-      });
 
       adDivs2.forEach((div) => {
         if (div.parentNode) {
--- weather-script.js
+++ weather-script.js
@@ -1,12 +1,6 @@
-console.log("**** Weather Clean-Up Script ****");
-
-// Select all <div> elements that have a class containing "ad-leaderboard" or "div-gpt-ad-topbanner"
 const adDivs = document.querySelectorAll(
   'div[class*="ad-"], div[class*="gpt-ad"], div[class*="glacier-ad"], div[class*="video_player"], div[id*="ad-"], div[id*="gpt-ad"], div[id*="video_player"], div[id*="taboola-module"]'
 );
-
-// Log the found elements to the console
-adDivs.forEach((div) => console.log(div));
 
 adDivs.forEach((div) => {
   if (div.parentNode) {
